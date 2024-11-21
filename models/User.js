324654b4097bb6711b82e071dd2951e89d9226d5
zxcrowzz <pantsbro4@gmail.com>
2024-11-20 16:06:03 -0800

    Update User.js

--- models/User.js
+++ models/User.js
@@ -2,11 +2,10 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
- 
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
+  profileImage: { type: String }, // URL or filename of profile image
   isVerified: { type: Boolean, default: false },
-  friends: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }]
 });
 
 module.exports = mongoose.model('User', userSchema);
