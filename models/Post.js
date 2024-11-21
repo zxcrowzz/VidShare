0b04dbb895d1b36656e413c51e9ebae0ecefae09
zxcrowzz <pantsbro4@gmail.com>
2024-11-20 16:05:40 -0800

    Update Post.js

--- models/Post.js
+++ models/Post.js
@@ -1,1 +1,9 @@
+const postSchema = new mongoose.Schema({
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  content: { type: String, required: true },
+  mediaUrl: { type: String }, // URL of the attached media
+  timestamp: { type: Date, default: Date.now },
+});
+
+module.exports = mongoose.model('Post', postSchema);
 
