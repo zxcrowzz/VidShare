af762bd2d994f41300ca720c0c20118204fb9754
zxcrowzz <pantsbro4@gmail.com>
2024-11-20 17:00:29 -0800

    Update Post.js

--- models/Post.js
+++ models/Post.js
@@ -1,3 +1,6 @@
+
+const mongoose = require('mongoose');
+
 const postSchema = new mongoose.Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   content: { type: String, required: true },
