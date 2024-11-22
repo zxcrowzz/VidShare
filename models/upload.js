416ae136ed94b5504f87a66ee8acebb538e02017
zxcrowzz <pantsbro4@gmail.com>
2024-11-21 18:09:36 -0800

    Update upload.js

--- models/upload.js
+++ models/upload.js
@@ -1,1 +1,8 @@
+const multer = require('multer');
+
+// Multer configuration for file upload
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
+
+module.exports = upload;
 
