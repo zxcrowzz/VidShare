ef83a73933fc980a16ee071519a1912c5299501b
zxcrowzz <pantsbro4@gmail.com>
2024-11-21 18:13:54 -0800

    Update User.js

--- models/User.js
+++ models/User.js
@@ -4,7 +4,7 @@ const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  profileImage: { type: String }, // URL or filename of profile image
+  profileImageUrl: { type: String }, // URL or filename of profile image
   isVerified: { type: Boolean, default: false },
 });
 
